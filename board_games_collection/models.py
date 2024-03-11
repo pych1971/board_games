b28e4377a1cbcb8d4d3f7591774cbd43ebd898c0
pych1971 <pych1971@gmail.com>
2024-03-11 16:03:46 +0500

    2024-03-11-16-03

--- board_games_collection/models.py
+++ board_games_collection/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class BoardGames(models.Model):
+    tesera_id = models.IntegerField()
+    tesera_name = models.CharField(max_length=1024)
+    tesera_rating_user = models.FloatField()
+    tesera_n10_rating = models.FloatField()
+    bgg_id = models.IntegerField()
+    bgg_name = models.CharField(max_length=1024)
+    bgg_average_rating = models.FloatField()
+    bgg_bayes_average_rating = models.FloatField()
+    bgg_rank = models.IntegerField()
+    bgg_weight = models.FloatField()
